dd9066c374e108a9935821cc6d25b9850f6825f7
JoaoDias <joaodiasworking@gmail.com>
2021-03-21 18:31:22 -0300

    St.

---
+++ Csharp_Begin/Estudos/Inicio/CourseParams/CourseParams/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CourseParams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int s1 = Calculator.Sum(2, 3);
+            int s2 = Calculator.Sum(2, 4, 3);
+
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+
+            int a = 10;
+            int triple;
+            Calculator.Triple(a, out triple);
+            Console.WriteLine(triple);
+        }
+    }
+}
